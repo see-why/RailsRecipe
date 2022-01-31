ccae0af522835cd2e167f1d8135f3a784a4e99ba
AmineHLub <Amino.bhl@gmail.com>
2022-01-31 22:26:49 +0100

    Add devise fully( including db:email)

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2022_01_31_151051) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
