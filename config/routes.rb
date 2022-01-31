411024944fefe5c97c863b702b21e63693a5960d
AmineHLub <Amino.bhl@gmail.com>
2022-01-31 22:26:16 +0100

    re-adjust routers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  devise_for :users
+
+  devise_scope :user do
+    root 'users#index'
   resources :users, only: [:index]
   resources :foods, only: [:index, :new, :create, :destroy]
   resources :recipes, only: [:index, :new, :create, :show, :destroy] do
     resources :recipe_foods, only:  [:create, :destroy]
   end
   resources :public_recipes, only: [:index]
-  resources :general_shopping_list, only: [:index]
-end
 
+  resources :general_shopping_list, only: [:index]
+  end
+end
