479a5880028fcb0b2eacce8878c296f11ad76d95
see-why <iyadicy@gmail.com>
2022-02-02 22:14:28 +0100

    Add destroy methods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -14,13 +14,23 @@ class FoodsController < ApplicationController
   def create
     new_food = current_user.foods.new(food_params)
     new_food.user_id = current_user.id
-    binding.pry
     if new_food.save
       redirect_to '/foods', notice: 'Success!'
     else
       render :new, alert: 'Error occured!'
     end
   end
+
+  def destroy
+    @food = Food.find(params[:id])
+    if @food.destroy
+      redirect_to '/foods', notice: 'Successfully deleted!'
+    else
+      redirect_to '/foods', alert: 'Error occured!'
+    end
+  end
+
+  private
 
   def food_params
     params.require(:food).permit(:name, :measurement_unit, :price)
