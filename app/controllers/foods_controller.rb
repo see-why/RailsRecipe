7d6189dc07bbe8fc2c8e92c835c2c603be0bb8b9
see-why <iyadicy@gmail.com>
2022-02-02 21:21:56 +0100

    Add action methods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,28 @@
 class FoodsController < ApplicationController
+  def index
+    @user = User.find(current_user.id)
+    @foods = @user.foods
+  end
+
+  def new
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def create
+    new_food = current_user.foods.new(food_params)
+    new_food.user_id = current_user.id
+    binding.pry
+    if new_food.save
+      redirect_to '/foods', notice: 'Success!'
+    else
+      render :new, alert: 'Error occured!'
+    end
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
 end
