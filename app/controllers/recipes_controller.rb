35d48472bd65eae84fdf7b3d92d828bbc6d9a681
AmineHLub <amino.bhl@gmail.com>
2022-02-02 06:28:05 +0100

    Add recipe controller data

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,17 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @recipes = current_user.recipes
+  end
+
+  def show
+    @recipe = current_user.recipes.find(params[:id])
+  end
+
+  def new; end
+
+  def create
+
+  end
 end
