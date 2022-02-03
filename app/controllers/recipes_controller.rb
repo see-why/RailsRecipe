6c9ac8816f8c45b67688edc428d41ef46eb45bb6
AmineHLub <amino.bhl@gmail.com>
2022-02-03 02:06:57 +0100

    Add adding recipe functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,6 +12,18 @@ class RecipesController < ApplicationController
   def new; end
 
   def create
+    new_recipe = current_user.recipes.new(recipe_params)
+    new_recipe.user_id = current_user.id
+    if new_recipe.save
+      redirect_to '/recipes', notice: 'Success!'
+    else
+      render :new, alert: 'Error occured!'
+    end
+  end
 
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :prep_time, :cook_time, :description, :public)
   end
 end
