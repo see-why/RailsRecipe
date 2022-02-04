73318084f7dfa295ac1d2aa31f88b4c004d95cbc
see-why <iyadicy@gmail.com>
2022-02-04 14:37:04 +0100

    Order by newest

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    @public_recipes = Recipe.where('public = true').includes(:foods).includes(:user)
+    @public_recipes = Recipe.where('public = true').order(id: :desc).includes(:foods).includes(:user)
   end
 end
