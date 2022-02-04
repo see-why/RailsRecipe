4f5c649cf4b47035056b6a0b121ee769361d17fc
see-why <iyadicy@gmail.com>
2022-02-04 07:09:19 +0100

    Add redirect to log in page for log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def index; end
+
+  protected
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to new_user_session_path
+    end
+  end
 end
